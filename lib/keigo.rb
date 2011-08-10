c60ac76639792112e7e552b2d909f73fc8c117d8
kano4 <shinji@kano4.com>
2011-08-10 22:42:46 +0900

    add: app_id and RuntimeError

--- lib/keigo.rb
+++ lib/keigo.rb
@@ -4,7 +4,21 @@ def sama name
 end
 
 class Keigo
-  def sama(name)
+  @@app_id = nil
+
+  def initialize
+    raise RuntimeError unless Keigo.app_id
+  end
+
+  def self.app_id
+    @@app_id
+  end
+
+  def self.app_id= app_id
+    @@app_id = app_id
+  end
+
+  def sama name
     "#{name}様"
   end
 end
--- spec/keigo_spec.rb
+++ spec/keigo_spec.rb
@@ -2,7 +2,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe 'Keigo' do
+  context 'application idがない場合' do
+    it { Keigo.app_id.should be_eql nil }
+    it { lambda{ Keigo.new }.should raise_error RuntimeError }
+    it { Object.private_methods.should be_include :sama}
+  end
+
   context 'application idがある場合' do
+    before(:each) do
+      Keigo.app_id = 1234
+    end
+
+    it { Keigo.app_id.should be_eql 1234 }
+
     it 'は、keigo.samaを使うと最後に"様"が付加される' do
       keigo = Keigo.new
       name = keigo.sama('Keigo')
