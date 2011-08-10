3a7a104eceaf6f4b075f21cbfba81ee129c559c5
kano4 <shinji@kano4.com>
2011-08-10 19:51:17 +0900

    add: sama spec in keigo_spec.rb

--- spec/keigo_spec.rb
+++ spec/keigo_spec.rb
@@ -1,7 +1,10 @@
+# encoding: utf-8
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Keigo" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "は、samaを使うと'様'が付加される" do
+    keigo = Keigo.new
+    name = keigo.sama('Keigo')
+    name.should == 'Keigo様'
   end
 end
