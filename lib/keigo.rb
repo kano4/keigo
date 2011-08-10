229b6946576bb6fe0b283d09d1728b69fc1a91b1
kano4 <shinji@kano4.com>
2011-08-10 21:35:08 +0900

    implement: sama function

--- lib/keigo.rb
+++ lib/keigo.rb
@@ -1,4 +1,8 @@
 # encoding: utf-8
+def sama name
+  Keigo.new.sama name
+end
+
 class Keigo
   def sama(name)
     "#{name}様"
--- spec/keigo_spec.rb
+++ spec/keigo_spec.rb
@@ -1,10 +1,16 @@
 # encoding: utf-8
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe "Keigo" do
-  it "は、samaを使うと'様'が付加される" do
-    keigo = Keigo.new
-    name = keigo.sama('Keigo')
-    name.should == 'Keigo様'
+describe 'Keigo' do
+  context 'application idがある場合' do
+    it 'は、keigo.samaを使うと最後に"様"が付加される' do
+      keigo = Keigo.new
+      name = keigo.sama('Keigo')
+      name.should == 'Keigo様'
+    end
+
+    it 'は、sama関数を使うと最後に"様"が付加される' do
+      sama('Keigo').should be_eql 'Keigo様'
+    end
   end
 end
