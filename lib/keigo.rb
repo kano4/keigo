9fb63247f9da8350ef208977fbfea7fbf8323e8d
kano4 <shinji@kano4.com>
2011-10-12 10:36:26 +0900

    remove: Keigo class

--- lib/keigo.rb
+++ lib/keigo.rb
@@ -9,26 +9,3 @@ class String
     text
   end
 end
-
-def sama name
-  Keigo.new.sama name
-end
-
-def k text
-  Keigo.new.k text
-end
-
-class Keigo
-  def initialize
-    @keigo_henkan = $keigo_henkan
-  end
-
-  def sama name
-    "#{name}様"
-  end
-
-  def k text
-    @keigo_henkan.each_key {|key| text = text.sub(key, @keigo_henkan[key])}
-    text
-  end
-end
--- spec/keigo_spec.rb
+++ spec/keigo_spec.rb
@@ -17,34 +17,3 @@ describe String do
     it { 'あいつが今日来る。'.keigo.should be_eql 'あちらさまが本日おいでになる。' }
   end
 end
-
-describe 'Keigo' do
-  before(:each) do
-    @keigo = Keigo.new
-  end
-  it 'は、keigo.samaを使うと最後に"様"が付加される' do
-    name = @keigo.sama('Keigo')
-    name.should == 'Keigo様'
-  end
-
-  it 'は、sama関数を使うと最後に"様"が付加される' do
-    sama('Keigo').should be_eql 'Keigo様'
-  end
-
-  context 'は、変換後に登録されていない場合' do
-    it { k('こんにちわ').should be_eql 'こんにちわ' }
-    it { k('わたくし').should be_eql 'わたくし' }
-  end
-
-  context 'は、変換語に登録されている場合' do
-    @keigo_henkan = $keigo_henkan
-
-    @keigo_henkan.each do |key, keigo|
-      it { k(key).should be_eql keigo }
-    end
-
-    it { k('あいつが今日来る。').should be_eql 'あちらさまが本日おいでになる。' }
-
-    it { @keigo.k('わたし').should be_eql 'わたくし' }
-  end
-end
